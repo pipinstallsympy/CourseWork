f5e7a85f9ba2384bc9eec139364cefcdf1b83faa
pipinstallsympy <misha.zherbin.05@gmail.com>
2025-05-28 14:24:27 +0300

    Добавил генерацию пор через методы

--- CourseWorkZherbin/CubeLine.cs
+++ CourseWorkZherbin/CubeLine.cs
@@ -19,4 +19,53 @@ public class CubeLine
             }
         }
     }
+
+    public void GeneratePoresByCount(int poreAmount)
+    {
+        int poreCount = 0;
+        int randIndex;
+        int len = Line.Count;
+        Random rand = new Random();
+
+        if (poreAmount < 0 || poreAmount >= len)
+        {
+            throw new ArgumentException();
+        }
+        while(poreAmount != poreCount)
+        {
+            randIndex = rand.Next(len);
+            if (this.Line[randIndex].IsEmpty == false)
+            {
+                this.Line[randIndex].IsEmpty = true;
+                poreCount++;
+            }
+        }
+    }
+
+    public void GeneratePoresByPersent(double persent)
+    {
+        if (persent < 0 || persent >= 100)
+        {
+            throw new ArgumentException();
+        }
+        
+        int poreCount = 0;
+        double porePersent = 0;
+        int randIndex;
+        int len = Line.Count;
+        Random rand = new Random();
+
+
+        while(porePersent < persent)
+        {
+            randIndex = rand.Next(len);
+            if (this.Line[randIndex].IsEmpty == false)
+            {
+                this.Line[randIndex].IsEmpty = true;
+                poreCount++;
+                porePersent = poreCount /len * 100;
+            }
+        }
+        
+    }
 }
--- CourseWorkZherbin/Figure.cs
+++ CourseWorkZherbin/Figure.cs
@@ -2,5 +2,5 @@
 
 public abstract class Figure
 {
-    protected bool IsEmpty = false;
+    public bool IsEmpty = false;
 }
